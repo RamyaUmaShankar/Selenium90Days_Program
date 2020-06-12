667332707a7808a39b0d1cdd35b59fb54d05424c
RamyaUmaShankar <65245718+RamyaUmaShankar@users.noreply.github.com>
2020-06-12 17:18:50 +0530

    Add files via upload

---
+++ Cucumber/steps/Bigbasket.java
@@ -0,0 +1,87 @@
+package steps;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class Bigbasket {
+	
+	
+
+@Given("Go to https:\\/\\/www.bigbasket.com\\/")
+public void goToHttpsWwwBigbasketCom() {
+    
+}
+
+@Given("mouse over on  Shop by Category")
+public void mouseOverOnShopByCategory() {
+    
+}
+
+@Given("Go to FOODGRAINS, OIL & MASALA and RICE & RICE PRODUCTS")
+public void goToFOODGRAINSOILMASALAAndRICERICEPRODUCTS() {
+    
+}
+
+@Given("Click on BOILED & STEAM RICE")
+public void clickOnBOILEDSTEAMRICE() {
+    
+}
+
+@Given("Get the URL of the page and check with site navigation link HOME, FOODGRAINS, OIL & MASALA> RICE & RICE PRODUCTS> BOILED & STEAM RICE")
+public void getTheURLOfThePageAndCheckWithSiteNavigationLinkHOMEFOODGRAINSOILMASALARICERICEPRODUCTSBOILEDSTEAMRICE() {
+   
+}
+
+@Given("Choose the Brand as bb Royal")
+public void chooseTheBrandAsBbRoyal() {
+   
+}
+
+@Given("Go to Ponni Boiled Rice and select {int}kg bag from Dropdown")
+public void goToPonniBoiledRiceAndSelectKgBagFromDropdown(Integer int1) {
+   
+}
+
+@Given("Click Add button")
+public void clickAddButton() {
+   
+}
+
+@Given("Go to search box and type Dal")
+public void goToSearchBoxAndTypeDal() {
+   
+}
+
+@Given("Add Toor\\/Arhar Dal {int}kg and set Qty Two from the list")
+public void addToorArharDalKgAndSetQtyTwoFromTheList(Integer int1) {
+   
+}
+
+@Given("click Address")
+public void clickAddress() {
+    
+}
+
+@Given("Select CHennai as City, Alandur{int},Chennai as Area  and click Continue")
+public void selectCHennaiAsCityAlandurChennaiAsAreaAndClickContinue(Integer int1) {
+   
+}
+
+@Given("Mouse over on My Basket take a screen shot")
+public void mouseOverOnMyBasketTakeAScreenShot() {
+  
+}
+
+@When("Click View Basket and Checkout")
+public void clickViewBasketAndCheckout() {
+    
+}
+
+@Then("Click the close button and close the browser")
+public void clickTheCloseButtonAndCloseTheBrowser() {
+  
+}
+
+
+}
